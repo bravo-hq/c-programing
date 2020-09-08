d2be7dce02d796f001dcfae2887f73590c037090
Coursera Learner <nobody@nowhere.nul>
2020-09-08 16:03:48 +0000

    seems done

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -16,26 +16,30 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
-int main(int argc, char ** argv) {
+void readData(FILE * f){
   char ** line=NULL;
   char * curr=NULL;
   size_t sz;  
-  size_t i=0;
+  int i=0;
   
+  while(getline(&curr,&sz,f)>=0){
+    line=realloc(line,(i+1)*sizeof(*line));
+    line[i]=curr;
+    curr=NULL;
+    i=i+1;
+    }
+  free(curr);
+  sortData(line,i);
+  for (size_t j=0;j<i;j++){
+    printf("%s",line[j]);
+    free(line[j]);
+  }
+  free(line);
+}
+
+int main(int argc, char ** argv){  
   if (argc==1){
-    while(getline(&curr,&sz,stdin)>=0){
-      line=realloc(line,(i+1)*sizeof(*line));
-      line[i]=curr;
-      curr=NULL;
-      i++;
-    }
-    free(curr);
-    sortData(line,i);
-    for (size_t j=0;j<i;j++){
-      printf("%s",line[j]);
-      free(line[j]);
-    }
-    free(line);
+    readData(stdin);
     return EXIT_SUCCESS;
   }
 
@@ -43,27 +47,15 @@ int main(int argc, char ** argv) {
     for (size_t h=1; h<argc; h++){
       FILE * f= fopen(argv[h],"r");
       if (f==NULL){
-	perror("could not open the file");
+	perror("could not open the file\n");
 	return EXIT_FAILURE;
       }
-      while(getline(&curr,&sz,f)>=0){
-	line=realloc(line,(i+1)*sizeof(*line));
-	line[i]=curr;
-	curr=NULL;
-	i++;
+      readData(f);
+      if (fclose(f)!=0){
+	perror("could not close the file\n");
+	return EXIT_FAILURE;
       }
-      sortData(line,i);
-      for (size_t j=0;j<i;j++){
-	printf("%s",line[j]);
-	free(line[j]);
-      }
-      i=0;
-      //    curr=NULL;
-      // line=NULL;
     }
-    free(curr);
-    free(line);
-  } 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
+  }
 }
-
