48c0eaa2433f3063e5c35ecece64c19ebbf749d5
Coursera Learner <nobody@nowhere.nul>
2020-09-12 17:23:07 +0000

    done

--- c4prj1_deck/test_c4deck.c
+++ c4prj1_deck/test_c4deck.c
@@ -4,4 +4,31 @@
 #include "deck.h"
 #include "cards.h"
 #include <string.h>
-#include <assert.h>
+
+int main (){
+  // printf("hello\n");
+  deck_t * deck = malloc(sizeof(*deck));
+    deck->n_cards=3;
+    
+    card_t c =card_from_num(30);
+    card_t a =card_from_num(10);
+    card_t b =card_from_num(20);
+    deck->cards=malloc(deck->n_cards*sizeof(*deck->cards));
+     deck->cards[0]=&a;
+    deck->cards[1]=&b;
+    deck->cards[2]=&c;
+    // add_card_to(deck,card_from_num(40));
+    // add_card_to(deck,card_from_num(50));
+    card_t * p=add_empty_card(deck);
+    printf("%p\n",(void *)p);
+    print_hand(deck);
+    printf("\n");
+    print_card(*p);
+    deck_t *new=make_deck_exclude(deck);
+    printf("\n");
+    printf("%d\n",(int)new->n_cards);
+    print_hand(new);
+    // printf("%d",c.suit);
+    // card_t * c1=add_empty_card(
+ return EXIT_SUCCESS;
+}
