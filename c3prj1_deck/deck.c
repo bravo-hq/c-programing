1539d9c2b498bdc7ee7b170db631ccc1c1ec19c6
Coursera Learner <nobody@nowhere.nul>
2020-09-12 12:10:46 +0000

    seems done

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -42,3 +42,59 @@ void assert_full_deck(deck_t * d) {
     temp=0;
   }
 }
+
+void add_card_to (deck_t * deck,card_t c){
+  deck->n_cards++;
+  deck->cards=realloc(deck->cards,deck->n_cards*sizeof(*deck->cards));
+  *deck->cards[deck->n_cards-1]=c;
+}
+
+card_t * add_empty_card(deck_t * deck){
+  card_t c;
+  c.value=0;
+  c.suit=0;
+  add_card_to(deck,c);
+  return deck->cards[deck->n_cards-1];
+}
+
+deck_t * make_deck_exclude(deck_t * excluded_cards){
+  deck_t * ans=malloc(sizeof(*ans));
+  int check=0;
+  for (unsigned i=0;i<52;i++){
+    card_t c=card_from_num(i);
+    for (unsigned j=0;j<excluded_cards->n_cards,j++){
+      if (c.value==excluded_cards->cards[j]->value && c.suit==excluded_cards->cards[j]->suit){
+	check=1;
+	break;
+      }     
+    }
+    if (check=1) {
+      check=0;
+      continue;
+    }
+    add_card_to(ans,c);
+  }
+  return ans;
+}
+
+deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands){
+  deck_t * exclude=malloc(sizeof(*exclude));
+  exclude->n_cards=0;
+  exclude->cards=NULL;
+  for (size_t i=0;i<n_hands;i++){
+    deck_t * h=hands[i];
+    for (size_t j=0;i<h->n_cards;j++){
+      add_card_to(exclude,h->cards[j]);
+    }
+  }
+  deck_t * ans=make_deck_exclude(exclude);
+  free_deck(exclude);
+  return ans;
+}
+void free_deck(deck_t * deck){
+  for (size_t i=0;i<deck->n_cards;i++){
+    free(deck->cards[i]);
+  }
+  free(deck->cards);
+  free(deck);
+}
--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -238,7 +238,20 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
 //implementation in eval-c4.o) so that the
 //other functions we have provided can make
 //use of get_match_counts.
-unsigned * get_match_counts(deck_t * hand) ;
+unsigned * get_match_counts(deck_t * hand){
+  unsigned * ans=calloc(hand->n_cards*sizeof(*ans));
+  for (size_t i=0;i<hand->n_hands;i++){
+    card_t h1=*hand->cards[i];
+    for (size_t j=0;j<hand->n_hands;j++){
+      card_t h2=*hand->cards[j];
+      if (h1.value==h2.value){
+	ans[i]++;
+      }
+    }
+  }
+  return ans;
+
+}
 
 // We provide the below functions.  You do NOT need to modify them
 // In fact, you should not modify them!
