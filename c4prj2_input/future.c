0f9a4983553722d777698d20295787da2d5dade8
Coursera Learner <nobody@nowhere.nul>
2020-09-13 14:59:20 +0000

    done

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -8,24 +8,24 @@
 
 void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
   if (fc->n_decks<index+1){
+    size_t temp=fc->n_decks;
     fc->decks=realloc(fc->decks,(index+1)*sizeof(*fc->decks));
     fc->n_decks=index+1;
     fc->decks[index].n_cards=0;
     fc->decks[index].cards=NULL;
-    for (size_t i=0;i<fc->n_decks;i++){
-      if (fc->decks[i].n_cards<1){
-	fc->decks[i].cards=NULL;
-      }
+    for (size_t i=temp;i<fc->n_decks;i++){
+     	fc->decks[i].cards=NULL;
+	fc->decks[index].n_cards=0;
     }
   }else{
-    fc->decks=realloc(fc->decks,fc->n_decks*sizeof(*fc->decks));
+        fc->decks=realloc(fc->decks,fc->n_decks*sizeof(*fc->decks));
   }
   fc->decks[index].n_cards++;
   fc->decks[index].cards=realloc(fc->decks[index].cards,fc->decks[index].n_cards*sizeof(card_t *));
   fc->decks[index].cards[fc->decks[index].n_cards-1]=ptr;
 }
 
-/*void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
+void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
   if (fc->n_decks==0 || fc->n_decks > deck->n_cards){
     perror("no future decks");
     return;
@@ -37,7 +37,7 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr){
     }
   }
 }
-*/
+/*
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
   if(fc->n_decks>deck->n_cards){
     fprintf(stderr,"Unknown>Available cards to draw from");
@@ -51,3 +51,4 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc){
       (*(fc->decks[i].cards[x])).value=(*(deck->cards[i])).value;
       (*(fc->decks[i].cards[x])).suit=(*(deck->cards[i])).suit;
     }}}
+*/
